8a0d34c288a5bbd8f753df918d42b7f4e25c611c
unknown <kolyshkin@.sqlmaze.local>
2011-05-04 13:22:50 +0400

    Fix error «Call-time pass-by-reference has been deprecated»

--- library/Respect/Validation/Rules/Call.php
+++ library/Respect/Validation/Rules/Call.php
@@ -7,7 +7,7 @@ class Call extends AbstractRelated
 
     public function getReferenceValue($input)
     {
-        return call_user_func($this->reference, &$input);
+        return call_user_func_array($this->reference, array(&$input));
     }
 
     public function hasReference($input)
