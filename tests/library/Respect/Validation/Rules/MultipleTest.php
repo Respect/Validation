d1112896d0b77ccdf8c536ee58ec699f3e75b4d6
Jean Pimentel <jeanfap@gmail.com>
2011-09-13 23:46:18 -0300

    Added Multiple Validator

---
+++ tests/library/Respect/Validation/Rules/MultipleTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Respect\Validation\Rules;
+
+class MultipleTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider providerForMultiple
+     *
+     */
+    public function testMultiple($number, $input)
+    {
+        $multiple = new Multiple($number);
+        $this->assertTrue($multiple->assert($input));
+    }
+
+    /**
+     * @dataProvider providerForNotMultiple
+     * @expectedException Respect\Validation\Exceptions\MultipleException
+     */
+    public function testNotMultiple($number, $input)
+    {
+        $multiple = new Multiple($number);
+        $this->assertTrue($multiple->assert($input));
+    }
+
+    public function providerForMultiple()
+    {
+        return array(
+            array(5, 20),
+            array(5, 5),
+            array(5, 0),
+            array(5, -500),
+            array(1, 0),
+            array(1, 1),
+            array(1, 2),
+            array(1, 3),
+            array(0, 0), // Only 0 is multiple of 0
+        );
+    }
+
+    public function providerForNotMultiple()
+    {
+        return array(
+            array(5, 11),
+            array(5, 3),
+            array(5, -1),
+            array(3, 4),
+            array(10, -8),
+            array(10, 57),
+            array(10, 21),
+            array(0, 1),
+            array(0, 2),
+        );
+    }
+
+}
