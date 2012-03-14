55f217e47307b4071629411e39215810ec29afc3
Kleber Hamada Sato <kleberhs007@yahoo.com>
2012-03-14 16:40:22 -0300

    Fix exception messages
    Signed-off-by: Kleber Hamada Sato <kleberhs007@yahoo.com>

--- library/Respect/Validation/Exceptions/PrimeNumberException.php
+++ library/Respect/Validation/Exceptions/PrimeNumberException.php
@@ -7,10 +7,10 @@ class PrimeNumberException extends ValidationException
 
     public static $defaultTemplates = array(
         self::MODE_DEFAULT => array(
-            self::STANDARD => '{{name}} must be numeric and must be over zero',
+            self::STANDARD => '{{name}} must be a valid prime number',
         ),
         self::MODE_NEGATIVE => array(
-            self::STANDARD => '{{name}} must not be numeric and must not be zero ou under',
+            self::STANDARD => '{{name}} must not be a valid prime number',
         )
     );
 
