dcc94661b28eab52cdeccc2b81b71afde5dd3937
Alexandre Gaigalas <alexandre@gaigalas.net>
2012-05-24 12:07:35 -0300

    Added AlwaysValid and AlwaysInvalid validators for mocking and testing purpouses

---
+++ library/Respect/Validation/Rules/AlwaysInvalid.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Respect\Validation\Rules;
+
+class AlwaysInvalid extends AbstractRule
+{
+
+    public function validate($input)
+    {
+        return false;
+    }
+
+}
---
+++ library/Respect/Validation/Rules/AlwaysValid.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Respect\Validation\Rules;
+
+class AlwaysValid extends AbstractRule
+{
+
+    public function validate($input)
+    {
+        return true;
+    }
+
+}
