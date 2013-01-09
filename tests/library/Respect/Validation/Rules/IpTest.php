86f91ad9785259e13702255a4c42152277c21eb7
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2013-01-09 12:48:11 -0200

    Implemented IP range validation (#95)

--- tests/library/Respect/Validation/Rules/IpTest.php
+++ tests/library/Respect/Validation/Rules/IpTest.php
@@ -4,24 +4,27 @@ namespace Respect\Validation\Rules;
 
 class IpTest extends \PHPUnit_Framework_TestCase
 {
-
-    protected $ipValidator;
-
-    protected function setUp()
-    {
-        $this->ipValidator = new Ip;
-    }
-
     /**
      * @dataProvider providerForIp
      *
      */
     public function test_valid_ips_should_return_True($input, $options=null)
     {
-        $this->ipValidator->ipOptions = $options;
-        $this->assertTrue($this->ipValidator->validate($input));
-        $this->assertTrue($this->ipValidator->assert($input));
-        $this->assertTrue($this->ipValidator->check($input));
+        $ipValidator = new Ip($options);
+        $this->assertTrue($ipValidator->validate($input));
+        $this->assertTrue($ipValidator->assert($input));
+        $this->assertTrue($ipValidator->check($input));
+    }
+
+    /**
+     * @dataProvider providerForIpBetweenRange
+     */
+    public function test_ips_between_range_should_return_True($input, $networkRange)
+    {
+        $ipValidator = new Ip($networkRange);
+        $this->assertTrue($ipValidator->validate($input));
+        $this->assertTrue($ipValidator->assert($input));
+        $this->assertTrue($ipValidator->check($input));
     }
 
     /**
@@ -30,15 +33,48 @@ class IpTest extends \PHPUnit_Framework_TestCase
      */
     public function test_invalid_ips_should_throw_IpException($input, $options=null)
     {
-        $this->ipValidator->ipOptions = $options;
-        $this->assertFalse($this->ipValidator->validate($input));
-        $this->assertFalse($this->ipValidator->assert($input));
+        $ipValidator = new Ip($options);
+        $this->assertFalse($ipValidator->validate($input));
+        $this->assertFalse($ipValidator->assert($input));
+    }
+
+    /**
+     * @dataProvider providerForIpOutsideRange
+     * @expectedException Respect\Validation\Exceptions\IpException
+     */
+    public function test_ips_outside_range_should_return_False($input, $networkRange)
+    {
+        $ipValidator = new Ip($networkRange);
+        $this->assertFalse($ipValidator->validate($input));
+        $this->assertFalse($ipValidator->assert($input));
     }
 
     public function providerForIp()
     {
         return array(
             array('127.0.0.1'),
+        );
+    }
+
+    public function providerForIpBetweenRange()
+    {
+        return array(
+            array('127.0.0.1', '127.*'),
+            array('127.0.0.1', '127.0.*'),
+            array('127.0.0.1', '127.0.0.*'),
+            array('192.168.2.6', '192.168.*.6'),
+            array('192.168.2.6', '192.*.2.6'),
+            array('10.168.2.6', '*.168.2.6'),
+            array('192.168.2.6', '192.168.*.*'),
+            array('192.10.2.6', '192.*.*.*'),
+            array('192.168.255.156', '*'),
+            array('192.168.255.156', '*.*.*.*'),
+            array('127.0.0.1', '127.0.0.0-127.0.0.255'),
+            array('192.168.2.6', '192.168.0.0-192.168.255.255'),
+            array('192.10.2.6', '192.0.0.0-192.255.255.255'),
+            array('192.168.255.156', '0.0.0.0-255.255.255.255'),
+            array('220.78.173.2', '220.78.168/21'),
+            array('220.78.173.2', '220.78.168.0/21'),
         );
     }
 
@@ -51,6 +87,42 @@ class IpTest extends \PHPUnit_Framework_TestCase
             array('Foo'),
             array(''),
             array('192.168.0.1', FILTER_FLAG_NO_PRIV_RANGE),
+        );
+    }
+
+    public function providerForIpOutsideRange()
+    {
+        return array(
+            array('127.0.0.1', '127.0.1.*'),
+            array('192.168.2.6', '192.163.*.*'),
+            array('192.10.2.6', '193.*.*.*'),
+            array('127.0.0.1', '127.0.1.0-127.0.1.255'),
+            array('192.168.2.6', '192.163.0.0-192.163.255.255'),
+            array('192.10.2.6', '193.168.0.0-193.255.255.255'),
+            array('220.78.176.1', '220.78.168/21'),
+            array('220.78.176.2', '220.78.168.0/21'),
+        );
+    }
+
+    /**
+     * @dataProvider providerForInvalidRanges
+     * @expectedException Respect\Validation\Exceptions\ComponentException
+     */
+    public function test_invalid_range_should_raise_exception($range)
+    {
+        $o = new Ip($range);
+    }
+
+    public function providerForInvalidRanges()
+    {
+        return array(
+            array('192.168'),
+            array('asd'),
+            array('192.168.0.0-192.168.0.256'),
+            array('192.168.0.0-192.168.0.1/4'),
+            array('192.168.0/1'),
+            array('192.168.2.0/256.256.256.256'),
+            array('192.168.2.0/8.256.256.256'),
         );
     }
 
