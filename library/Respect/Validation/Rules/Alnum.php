f37e7ba23f976a5638aa5e99fccc19f18335f83a
Andre Ramaciotti <andre@ramaciotti.com>
2013-01-20 15:19:11 -0200

    Alpha refactoring to avoid code duplication
    As a bonus, Alnum also uses a ctype_ function now.

--- library/Respect/Validation/Rules/Alnum.php
+++ library/Respect/Validation/Rules/Alnum.php
@@ -5,7 +5,6 @@ namespace Respect\Validation\Rules;
 class Alnum extends Alpha
 {
 
-    public $additionalChars = '';
-    public $stringFormat = '/^(\s|[a-zA-Z0-9])*$/';
+    protected $ctype_func = 'ctype_alnum';
 
 }
