d711b62805516e4e3197519852d1cba9af5b00ca
nickl- <github@jigsoft.co.za>
2013-01-22 23:16:22 +0200

    Not filtering when none required just seems propper.

--- library/Respect/Validation/Rules/Space.php
+++ library/Respect/Validation/Rules/Space.php
@@ -3,7 +3,11 @@ namespace Respect\Validation\Rules;
 
 class Space extends AbstractCtypeRule
 {
-    public $additionalChars = '';
+    protected function filter($input)
+    {
+        return $input;
+    }
+
     protected function ctypeFunction($input)
     {
         return ctype_space($input);
