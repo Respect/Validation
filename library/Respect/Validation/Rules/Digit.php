55e24cdf2a2ee84db788311962788384fde0151e
nickl- <github@jigsoft.co.za>
2013-01-22 23:12:56 +0200

    Remove redundant  and filter is inherited

--- library/Respect/Validation/Rules/Digit.php
+++ library/Respect/Validation/Rules/Digit.php
@@ -3,7 +3,6 @@ namespace Respect\Validation\Rules;
 
 class Digit extends AbstractCtypeRule
 {
-    public $additionalChars = "\n\r\t ";
     protected function ctypeFunction($input)
     {
         return ctype_digit($input);
