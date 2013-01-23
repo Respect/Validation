79c079ea8be50cbc88f359322b03cd11261128b3
nickl- <github@jigsoft.co.za>
2013-01-23 09:56:44 +0200

    Improve CPF legibility and footprint.

--- library/Respect/Validation/Rules/Cpf.php
+++ library/Respect/Validation/Rules/Cpf.php
@@ -8,26 +8,18 @@ class Cpf extends AbstractRule
         // Code ported from jsfromhell.com
         $c = preg_replace('/\D/', '', $input);
 
-        if (strlen($c) != 11) {
+        if (strlen($c) != 11 || preg_match("/^{$c[0]}{11}$/", $c))
             return false;
-        }
-
-        if (preg_match("/^{$c[0]}{11}$/", $c)) {
-            return false;
-        }
 
         for ($s = 10, $n = 0, $i = 0; $s >= 2; $n += $c[$i++] * $s--);
 
-        if ($c[9] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+        if ($c[9] != ((($n %= 11) < 2) ? 0 : 11 - $n))
             return false;
-        }
 
         for ($s = 11, $n = 0, $i = 0; $s >= 2; $n += $c[$i++] * $s--);
 
-        if ($c[10] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+        if ($c[10] != ((($n %= 11) < 2) ? 0 : 11 - $n))
             return false;
-        }
-
         return true;
     }
 }
