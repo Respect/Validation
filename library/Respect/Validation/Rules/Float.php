2c60e4f38197eeb809bb6585eef3f235a024f3e7
Klederson Bueno <klederson@klederson.com>
2013-10-09 17:50:08 -0300

    Support Locales
    FLOAT now should support the current given locale once there's nowhere where you can specify it.

--- library/Respect/Validation/Rules/Float.php
+++ library/Respect/Validation/Rules/Float.php
@@ -5,7 +5,7 @@ class Float extends AbstractRule
 {
     public function validate($input)
     {
-        return is_float(filter_var($input, FILTER_VALIDATE_FLOAT));
+        $localeData = localeconv();
+        return is_float(filter_var($input, FILTER_VALIDATE_FLOAT, array("options"=>array('decimal' => $localeData['decimal_point']))));
     }
 }
-
