19d882c8588af838d8574927091fc843565de44e
flip111 <flip101@gmail.com>
2014-05-04 12:28:04 +0200

    notEmpty fix
    For strings only: notEmpty now means "there was content supplied", rather than "the content was not empty according to the php empty function".
    
    Effect: notEmpty no longer validates to false when string "0" is supplied.
    
    This make more sense when validating HTML forms.
    
    Related issue: https://github.com/Respect/Validation/issues/154

--- library/Respect/Validation/Rules/NotEmpty.php
+++ library/Respect/Validation/Rules/NotEmpty.php
@@ -6,7 +6,7 @@ class NotEmpty extends AbstractRule
     public function validate($input)
     {
         if (is_string($input)) {
-            $input = trim($input);
+            return trim($input) !== '';
         }
 
         return !empty($input);
