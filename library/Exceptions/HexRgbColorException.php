960732d26ce662bcf61103ef1b8533f5e523e4af
davidepastore <pasdavide@gmail.com>
2014-07-16 10:02:26 +0200

    Create HexRgbColor rule

---
+++ library/Exceptions/HexRgbColorException.php
@@ -0,0 +1,15 @@
+<?php
+namespace Respect\Validation\Exceptions;
+
+class HexRgbColorException extends ValidationException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a hex RGB color',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a hex RGB color',
+        )
+    );
+}
+
