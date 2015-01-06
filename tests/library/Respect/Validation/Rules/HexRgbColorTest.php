bc80506f4bfdfd25811e01b9e5ca12d75ecf3c43
davidepastore <pasdavide@gmail.com>
2015-01-06 16:04:00 +0100

    Use data providers for HexRgbColorTest.php.

--- tests/library/Respect/Validation/Rules/HexRgbColorTest.php
+++ tests/library/Respect/Validation/Rules/HexRgbColorTest.php
@@ -3,40 +3,57 @@ namespace Respect\Validation\Rules;
 
 class HexRgbColorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testHexRgbColorValuesONLYShouldReturnTrue()
+    /**
+     * @dataProvider providerForValidHexRgbColor
+     */
+    public function testHexRgbColorValuesONLYShouldReturnTrue($validHexRgbColor)
     {
         $validator = new HexRgbColor();
-        $this->assertTrue($validator->assert('#123456'));
-        $this->assertTrue($validator->assert('#123'));
-        $this->assertTrue($validator->assert('#FFFFFF'));
-        $this->assertTrue($validator->assert('123123'));
-        $this->assertTrue($validator->assert('FFFFFF'));
+        $this->assertTrue($validator->__invoke($validHexRgbColor));
+        $this->assertTrue($validator->check($validHexRgbColor));
+        $this->assertTrue($validator->assert($validHexRgbColor));
     }
 
     /**
+     * @dataProvider providerForInvalidHexRgbColor
      * @expectedException Respect\Validation\Exceptions\HexRgbColorException
      */
-    public function testInvalidHexRgbColorShouldRaiseException()
+    public function testInvalidHexRgbColorValuesShouldReturnFalse($invalidHexRgbColor)
     {
         $validator = new HexRgbColor();
-        $this->assertFalse($validator->check('#AAAAAA1'));
-        $this->assertFalse($validator->check('#S'));
+        $this->assertFalse($validator->__invoke($invalidHexRgbColor));
+        $this->assertFalse($validator->check($invalidHexRgbColor));
+        $this->assertFalse($validator->assert($invalidHexRgbColor));
     }
-
-    public function testInvalidHexRgbColorValuesShouldReturnFalse()
+    
+    public function providerForValidHexRgbColor()
     {
-        $validator = new HexRgbColor();
-        $this->assertFalse($validator->__invoke('foo'));
-        $this->assertFalse($validator->__invoke(new \stdClass()));
-        $this->assertFalse($validator->__invoke(array()));
-        $this->assertFalse($validator->__invoke(null));
-        $this->assertFalse($validator->__invoke('#0'));
-        $this->assertFalse($validator->__invoke('#1234'));
-        $this->assertFalse($validator->__invoke('1234'));
-        $this->assertFalse($validator->__invoke(1));
-        $this->assertFalse($validator->__invoke(005));
-        $this->assertFalse($validator->__invoke(443));
-        $this->assertFalse($validator->__invoke(000099));
+        return array(
+            array('#123456'),
+            array('#123'),
+            array('#FFFFFF'),
+            array('123123'),
+            array('FFFFFF')
+        );
+    }
+    
+    public function providerForInvalidHexRgbColor()
+    {
+        return array(
+            array('foo'),
+            array(new \stdClass()),
+            array(array()),
+            array(null),
+            array('#0'),
+            array('#1234'),
+            array('1234'),
+            array(1),
+            array(005),
+            array(443),
+            array(000099),
+            array('#AAAAAA1'),
+            array('#S')
+        );
     }
 }
 
