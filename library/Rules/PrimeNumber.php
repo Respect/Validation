a1283fb2c504fc7bb26c3476c926a5dd2b468cdb
kmilotxm@gmail.com <kmilotxm@gmail.com>
2015-01-23 14:25:03 -0200

    Performace optimize

--- library/Rules/PrimeNumber.php
+++ library/Rules/PrimeNumber.php
@@ -3,17 +3,20 @@ namespace Respect\Validation\Rules;
 
 class PrimeNumber extends AbstractRule
 {
-    public function validate($input){
-
-        if(!is_numeric($input) || $input <= 1)
+    public function validate($input)
+    {
+        if(!is_numeric($input) || $input <= 1){
             return false;
+        }
         
-        if($input!= 2 && $input % 2 ==  0)
+        if($input != 2 && ($input % 2) ==  0){
             return false;
+        }
         
         for($i=2; $i < $input; $i++){
-            if($input % $i == 0)
+            if(($input % $i) == 0){
                 return false;
+            }
         }
         
         return true;
