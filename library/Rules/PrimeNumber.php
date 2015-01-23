e9a8565723b729a6108da7569b9bee9c18c003be
Camilo Teixeira de Melo <kmilotxm@users.noreply.github.com>
2015-01-23 10:12:26 -0200

    Update PrimeNumber.php

--- library/Rules/PrimeNumber.php
+++ library/Rules/PrimeNumber.php
@@ -8,7 +8,8 @@ class PrimeNumber extends AbstractRule
         if(!is_numeric($input) || $input <= 0)
             return false;
 
-        if($input % 2 ==  0) return false;
+        if($input % 2 ==  0)
+            return false;
 
         for($i=2; $i < $input - 1; $i++){
             if($input % $i == 0)
