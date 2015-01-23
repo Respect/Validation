ffda46fdea296585945c7de52b8f2de1a2454e04
kmilotxm@gmail.com <kmilotxm@gmail.com>
2015-01-23 15:52:19 -0200

    Fix the coding standards problems.

--- library/Rules/PrimeNumber.php
+++ library/Rules/PrimeNumber.php
@@ -5,20 +5,20 @@ class PrimeNumber extends AbstractRule
 {
     public function validate($input)
     {
-        if(!is_numeric($input) || $input <= 1){
+        if (!is_numeric($input) || $input <= 1) {
             return false;
         }
-        
-        if($input != 2 && ($input % 2) ==  0){
+
+        if ($input != 2 && ($input % 2) ==  0) {
             return false;
         }
-        
-        for($i=2; $i < $input; $i++){
-            if(($input % $i) == 0){
+
+        for ($i = 2; $i < $input; $i++) {
+            if (($input % $i) == 0) {
                 return false;
             }
         }
-        
+
         return true;
     }
 }
