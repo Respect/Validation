f7fe245555b7d6b9b7003f728187023b449a39aa
Alexander Wühr <wuehr@sc-networks.com>
2015-02-06 18:11:46 +0100

    Make Roman rule internally use Regex rule

--- library/Rules/Roman.php
+++ library/Rules/Roman.php
@@ -1,10 +1,11 @@
 <?php
 namespace Respect\Validation\Rules;
 
-class Roman extends AbstractRule
+class Roman extends Regex
 {
-    public function validate($input)
+    public function __construct()
     {
-        return (boolean) preg_match('/^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/', $input);
+        $pattern = '^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$';
+        parent::__construct('/'.$pattern.'/');
     }
 }
