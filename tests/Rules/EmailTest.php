13c3c92ae621a357665f791604d923181772430a
Eduardo Gulias Davis <me@egulias.com>
2015-02-15 23:36:31 +0100

    Use "egulias/emailvalidator" for email validation

--- tests/Rules/EmailTest.php
+++ tests/Rules/EmailTest.php
@@ -1,8 +1,85 @@
 <?php
 namespace Respect\Validation\Rules;
 
+function class_exists($className)
+{
+    if (isset($GLOBALS['class_exists'][$className])) {
+        return $GLOBALS['class_exists'][$className];
+    }
+
+    return \class_exists($className);
+}
+
 class EmailTest extends \PHPUnit_Framework_TestCase
 {
+    private function setEmailValidatorExists($value)
+    {
+        $GLOBALS['class_exists']['Egulias\EmailValidator\EmailValidator'] = (bool) $value;
+    }
+
+    private function resetClassExists()
+    {
+        unset($GLOBALS['class_exists']);
+    }
+
+    private function getEmailValidatorMock()
+    {
+        $emailValidatorMock = $this
+            ->getMockBuilder('Egulias\\EmailValidator\\EmailValidator')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return $emailValidatorMock;
+    }
+
+    protected function setUp()
+    {
+        $this->setEmailValidatorExists(false);
+    }
+
+    protected function tearDown()
+    {
+        $this->resetClassExists();
+    }
+
+    public function testShouldAcceptInstanceOfEmailValidatorOnConstructor()
+    {
+        $this->resetClassExists();
+
+        $emailValidator = $this->getEmailValidatorMock();
+
+        $rule = new Email($emailValidator);
+
+        $this->assertSame($emailValidator, $rule->getEmailValidator());
+    }
+
+    public function testShouldHaveADefaultInstanceOfEmailValidator()
+    {
+        $this->resetClassExists();
+
+        $rule = new Email();
+
+        $this->assertInstanceOf('Egulias\\EmailValidator\\EmailValidator', $rule->getEmailValidator());
+    }
+
+    public function testShouldUseEmailValidatorWhenDefined()
+    {
+        $this->resetClassExists();
+
+        $input = 'example@example.com';
+
+        $emailValidator = $this->getEmailValidatorMock();
+        $emailValidator
+            ->expects($this->once())
+            ->method('isValid')
+            ->with($input)
+            ->will($this->returnValue(true));
+
+        $rule = new Email($emailValidator);
+
+        $this->assertTrue($rule->validate($input));
+    }
+
     /**
      * @dataProvider providerForValidEmail
      */
