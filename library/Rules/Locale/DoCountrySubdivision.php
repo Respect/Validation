49f7013170c8351bc8072f6545d336eff497e99e
Henrique Moody <henriquemoody@gmail.com>
2015-04-24 16:43:43 -0300

    Create country subdivision rule for "DO"

---
+++ library/Rules/Locale/DoCountrySubdivision.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Respect\Validation\Rules\Locale;
+
+use Respect\Validation\Rules\AbstractSearcher;
+
+/**
+ * Dominican Republic country subdivision.
+ *
+ * ISO 3166-1 alpha-2: DO
+ *
+ * @link http://www.geonames.org/DO/administrative-division-dominican-republic.html
+ */
+class DoCountrySubdivision extends AbstractSearcher
+{
+    public $haystack = array(
+        '01', // Distrito Nacional
+        '02', // Azua
+        '03', // Baoruco
+        '04', // Barahona
+        '05', // Dajabon
+        '06', // Duarte
+        '07', // Elias Pina
+        '08', // El Seybo
+        '09', // Espaillat
+        '10', // Independencia
+        '11', // La Altagracia
+        '12', // La Romana
+        '13', // La Vega
+        '14', // Maria Trinidad Sanchez
+        '15', // Monte Cristi
+        '16', // Pedernales
+        '17', // Peravia (Bani)
+        '18', // Puerto Plata
+        '19', // Salcedo
+        '20', // Samana
+        '21', // San Cristobal
+        '22', // San Juan
+        '23', // San Pedro de Macoris
+        '24', // Sanchez Ramirez
+        '25', // Santiago
+        '26', // Santiago Rodriguez
+        '27', // Valverde
+        '28', // Monsenor Nouel
+        '29', // Monte Plata
+        '30', // Hato Mayor
+    );
+
+    public $compareIdentical = true;
+}
