a3c945aa51041b7a479ee9482c69a0f059fc228e
Henrique Moody <henriquemoody@gmail.com>
2015-06-08 12:09:25 -0300

    Add license information and copyright in all files

--- library/Rules/Executable.php
+++ library/Rules/Executable.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
 namespace Respect\Validation\Rules;
 
 class Executable extends AbstractRule
