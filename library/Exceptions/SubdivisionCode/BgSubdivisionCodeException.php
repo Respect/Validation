8f369a999d3cbd08a53c9e762ec3979591855094
Henrique Moody <henriquemoody@gmail.com>
2015-09-21 13:26:39 -0300

    Create "BgSubdivisionCode" rule

---
+++ library/Exceptions/SubdivisionCode/BgSubdivisionCodeException.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Exceptions\SubdivisionCode;
+
+use Respect\Validation\Exceptions\SubdivisionCodeException;
+
+/**
+ * Exception class for Bulgaria subdivision code.
+ *
+ * ISO 3166-1 alpha-2: BG
+ */
+class BgSubdivisionCodeException extends SubdivisionCodeException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a subdivision code of Bulgaria',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a subdivision code of Bulgaria',
+        ),
+    );
+}
