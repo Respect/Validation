2949c3502905e155a83912190af3f714dbb0c6d8
Henrique Moody <henriquemoody@gmail.com>
2015-09-21 13:27:09 -0300

    Create "JmSubdivisionCode" rule

---
+++ library/Exceptions/SubdivisionCode/JmSubdivisionCodeException.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Exceptions\SubdivisionCode;
+
+use Respect\Validation\Exceptions\SubdivisionCodeException;
+
+/**
+ * Exception class for Jamaica subdivision code.
+ *
+ * ISO 3166-1 alpha-2: JM
+ */
+class JmSubdivisionCodeException extends SubdivisionCodeException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a subdivision code of Jamaica',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a subdivision code of Jamaica',
+        ),
+    );
+}
