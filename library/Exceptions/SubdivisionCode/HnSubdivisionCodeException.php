1d28bb8b82282f5248a6e91d8f5f88d9cf3b6792
Henrique Moody <henriquemoody@gmail.com>
2015-09-21 13:27:04 -0300

    Create "HnSubdivisionCode" rule

---
+++ library/Exceptions/SubdivisionCode/HnSubdivisionCodeException.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Exceptions\SubdivisionCode;
+
+use Respect\Validation\Exceptions\SubdivisionCodeException;
+
+/**
+ * Exception class for Honduras subdivision code.
+ *
+ * ISO 3166-1 alpha-2: HN
+ */
+class HnSubdivisionCodeException extends SubdivisionCodeException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a subdivision code of Honduras',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a subdivision code of Honduras',
+        ),
+    );
+}
