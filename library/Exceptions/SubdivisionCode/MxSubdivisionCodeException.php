5c18951ca0ac025cc6dd8177a3e57e48fd191577
Henrique Moody <henriquemoody@gmail.com>
2015-09-21 13:27:26 -0300

    Create "MxSubdivisionCode" rule

---
+++ library/Exceptions/SubdivisionCode/MxSubdivisionCodeException.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Exceptions\SubdivisionCode;
+
+use Respect\Validation\Exceptions\SubdivisionCodeException;
+
+/**
+ * Exception class for Mexico subdivision code.
+ *
+ * ISO 3166-1 alpha-2: MX
+ */
+class MxSubdivisionCodeException extends SubdivisionCodeException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a subdivision code of Mexico',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a subdivision code of Mexico',
+        ),
+    );
+}
