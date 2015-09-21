a0698cf8a10fc3fbb646015b660a615822020a45
Henrique Moody <henriquemoody@gmail.com>
2015-09-21 13:27:11 -0300

    Create "KhSubdivisionCode" rule

---
+++ library/Exceptions/SubdivisionCode/KhSubdivisionCodeException.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Exceptions\SubdivisionCode;
+
+use Respect\Validation\Exceptions\SubdivisionCodeException;
+
+/**
+ * Exception class for Cambodia subdivision code.
+ *
+ * ISO 3166-1 alpha-2: KH
+ */
+class KhSubdivisionCodeException extends SubdivisionCodeException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a subdivision code of Cambodia',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a subdivision code of Cambodia',
+        ),
+    );
+}
