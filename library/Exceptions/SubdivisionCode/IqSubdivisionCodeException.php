103c9de8a59eeca65b05306c4228db333885c3d3
Henrique Moody <henriquemoody@gmail.com>
2015-09-21 13:27:07 -0300

    Create "IqSubdivisionCode" rule

---
+++ library/Exceptions/SubdivisionCode/IqSubdivisionCodeException.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Exceptions\SubdivisionCode;
+
+use Respect\Validation\Exceptions\SubdivisionCodeException;
+
+/**
+ * Exception class for Iraq subdivision code.
+ *
+ * ISO 3166-1 alpha-2: IQ
+ */
+class IqSubdivisionCodeException extends SubdivisionCodeException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a subdivision code of Iraq',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a subdivision code of Iraq',
+        ),
+    );
+}
