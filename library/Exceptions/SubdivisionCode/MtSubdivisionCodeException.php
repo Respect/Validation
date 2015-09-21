0e41f23a0e5e268b0f3944a0062e7ddf0bf4576b
Henrique Moody <henriquemoody@gmail.com>
2015-09-21 13:27:24 -0300

    Create "MtSubdivisionCode" rule

---
+++ library/Exceptions/SubdivisionCode/MtSubdivisionCodeException.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Exceptions\SubdivisionCode;
+
+use Respect\Validation\Exceptions\SubdivisionCodeException;
+
+/**
+ * Exception class for Malta subdivision code.
+ *
+ * ISO 3166-1 alpha-2: MT
+ */
+class MtSubdivisionCodeException extends SubdivisionCodeException
+{
+    public static $defaultTemplates = array(
+        self::MODE_DEFAULT => array(
+            self::STANDARD => '{{name}} must be a subdivision code of Malta',
+        ),
+        self::MODE_NEGATIVE => array(
+            self::STANDARD => '{{name}} must not be a subdivision code of Malta',
+        ),
+    );
+}
