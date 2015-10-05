4ce4c3a1cced1714fe40c8003982c0fae8ae4e6a
Ivan Zinovyev <vanyazin@gmail.com>
2015-10-05 23:04:38 +0300

    Nested key validation rules

--- library/Rules/NestedKey.php
+++ library/Rules/NestedKey.php
@@ -26,11 +26,49 @@ class NestedKey extends AbstractRelated
 
     public function hasReference($input)
     {
-        // TODO: Implement hasReference() method.
+        return !!$this->getReferenceValue($input);
     }
 
     public function getReferenceValue($input)
     {
-        // TODO: Implement getReferenceValue() method.
+        $type = gettype($input);
+        if (false === $pos = strpos($this->reference, '.')) {
+            if ('array' === $type && array_key_exists($this->reference, $input)) {
+                return $input[$this->reference];
+            } elseif ('object' === $type && property_exists($input, $this->reference)) {
+                return $input->{$this->reference};
+            } else {
+                throw new ComponentException(sprintf(
+                    'Can not select the key %s from the variable of type %s',
+                    $this->reference,
+                    $type
+                ));
+            }
+        } else {
+            $leftKey = substr($this->reference, 0, $pos);
+            $rightKey = substr($this->reference, $pos + 1);
+
+            if ('array' === $type && array_key_exists($leftKey, $input)) {
+                if ($rightKey) {
+                    $nestedKey = new static($rightKey, $this->validator, $this->mandatory);
+                    return $nestedKey->getReferenceValue($input[$leftKey]);
+                } else {
+                    return $input[$leftKey];
+                }
+            } elseif ('object' === $type && property_exists($input, $leftKey)) {
+                if ($rightKey) {
+                    $nestedKey = new static($rightKey, $this->validator, $this->mandatory);
+                    return $nestedKey->getReferenceValue($input->{$leftKey});
+                } else {
+                    return $input->{$leftKey};
+                }
+            } else {
+                throw new ComponentException(sprintf(
+                    'Can not select the key %s from the variable of type %s',
+                    $leftKey,
+                    $type
+                ));
+            }
+        }
     }
 }
--- tests/unit/Rules/NestedKeyTest.php
+++ tests/unit/Rules/NestedKeyTest.php
@@ -22,14 +22,59 @@ class NestedKeyTest extends \PHPUnit_Framework_TestCase
 {
     public function testArrayWithPresentKeysWillReturnTrue()
     {
-        $validator = new NestedKey('bar.foo.baz');
+        $fullPathValidator = new NestedKey('bar.foo.baz');
+        $halfPathValidator = new NestedKey('bar.foo');
+        $dirtyPathValidator = new NestedKey('bar.foooo.');
         $obj = array(
             'bar' => array (
                 'foo' => array (
                     'baz' => 'hello world!',
                 ),
+                'foooo' => array (
+                    'boooo' => 321,
+                ),
             ),
         );
-        $validator->assert($obj);
+
+        $this->assertTrue($fullPathValidator->assert($obj));
+        $this->assertTrue($fullPathValidator->check($obj));
+        $this->assertTrue($fullPathValidator->validate($obj));
+
+        $this->assertTrue($halfPathValidator->assert($obj));
+        $this->assertTrue($halfPathValidator->check($obj));
+        $this->assertTrue($halfPathValidator->validate($obj));
+
+        $this->assertTrue($dirtyPathValidator->assert($obj));
+        $this->assertTrue($dirtyPathValidator->check($obj));
+        $this->assertTrue($dirtyPathValidator->validate($obj));
+    }
+
+    public function testObjectWithPresentPropertiesWillReturnTrue()
+    {
+        $fullPathValidator = new NestedKey('bar.foo.baz');
+        $halfPathValidator = new NestedKey('bar.foo');
+        $dirtyPathValidator = new NestedKey('bar.foooo.');
+        $obj = (object) array(
+            'bar' => (object) array (
+                'foo' => (object) array (
+                    'baz' => 'hello world!',
+                ),
+                'foooo' => (object) array (
+                    'boooo' => 321,
+                ),
+            ),
+        );
+
+        $this->assertTrue($fullPathValidator->assert($obj));
+        $this->assertTrue($fullPathValidator->check($obj));
+        $this->assertTrue($fullPathValidator->validate($obj));
+
+        $this->assertTrue($halfPathValidator->assert($obj));
+        $this->assertTrue($halfPathValidator->check($obj));
+        $this->assertTrue($halfPathValidator->validate($obj));
+
+        $this->assertTrue($dirtyPathValidator->assert($obj));
+        $this->assertTrue($dirtyPathValidator->check($obj));
+        $this->assertTrue($dirtyPathValidator->validate($obj));
     }
 }
