aa3606d2241bed4407182f3e1b3c96bb0a036757
Nawarian Nickolas <nawarian@phoenix>
2015-10-17 13:15:30 -0300

    trocando __invokes por validate()

--- tests/unit/Rules/LengthTest.php
+++ tests/unit/Rules/LengthTest.php
@@ -24,9 +24,7 @@ class LengthTest extends \PHPUnit_Framework_TestCase
     public function testLengthInsideBoundsShouldReturnTrue($string, $min, $max)
     {
         $validator = new Length($min, $max);
-        $this->assertTrue($validator->__invoke($string));
-        $this->assertTrue($validator->check($string));
-        $this->assertTrue($validator->assert($string));
+        $this->assertTrue($validator->validate($string));
     }
 
     /**
@@ -36,7 +34,7 @@ class LengthTest extends \PHPUnit_Framework_TestCase
     public function testLengthOutsideBoundsShouldThrowLengthException($string, $min, $max)
     {
         $validator = new Length($min, $max, false);
-        $this->assertfalse($validator->__invoke($string));
+        $this->assertfalse($validator->validate($string));
         $this->assertfalse($validator->assert($string));
     }
 
