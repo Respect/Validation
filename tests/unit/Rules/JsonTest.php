6f5f4f82d478ff8ce0773f895cb62123db0edd79
Emmerson <emmersonsiqueira@gmail.com>
2015-10-18 02:09:10 -0300

    #563 Improved Json Rule class

--- tests/unit/Rules/JsonTest.php
+++ tests/unit/Rules/JsonTest.php
@@ -36,6 +36,14 @@ class JsonTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @dataProvider providerForNotPass
+     */
+    public function testInvalidJsonsShouldReturnFalse($input)
+    {
+        $this->assertFalse($this->json->validate($input));
+    }
+
+    /**
      * @expectedException Respect\Validation\Exceptions\JSonException
      */
     public function testInvalidJsonsShouldThrowJsonException()
@@ -52,13 +60,19 @@ class JsonTest extends \PHPUnit_Framework_TestCase
     public function providerForPass()
     {
         return [
-            ['2'],
-            ['"abc"'],
             ['[1,2,3]'],
             ['["foo", "bar", "number", 1]'],
             ['{"foo": "bar", "number":1}'],
             ['[]'],
             ['{}'],
+        ];
+    }
+
+    public function providerForNotPass()
+    {
+        return [
+            ['2'],
+            ['"abc"'],
             ['false'],
             ['null'],
         ];
