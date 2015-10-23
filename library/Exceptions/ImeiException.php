9530a0b28ebb0885bdae9c65599d5c2f8cf1bd18
Diego Oliveira <contato@diegoholiveira.com>
2015-10-23 00:00:19 -0200

    Improve the error message

--- library/Exceptions/ImeiException.php
+++ library/Exceptions/ImeiException.php
@@ -2,7 +2,14 @@
 
 namespace Respect\Validation\Exceptions;
 
-
 class ImeiException extends ValidationException
 {
+    public static $defaultTemplates = [
+        self::MODE_DEFAULT => [
+            self::STANDARD => '{{input}} must be a valid IMEI',
+        ],
+        self::MODE_NEGATIVE => [
+            self::STANDARD => '{{input}} must not be a valid IMEI',
+        ],
+    ];
 }
