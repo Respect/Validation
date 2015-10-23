9c7960e3fe228ceb323a192f10e89037b46a2086
Diego Oliveira <contato@diegoholiveira.com>
2015-10-22 23:45:56 -0200

    Validation working

---
+++ library/Rules/Imei.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Respect\Validation\Rules;
+
+
+class Imei extends AbstractRule
+{
+    const IMEI_SIZE = 15;
+
+    public function __construct()
+    {
+        $this->name = 'imei';
+    }
+
+    public function validate($input)
+    {
+        if (!$input || strlen($input) != self::IMEI_SIZE) {
+            return false;
+        }
+
+        $numbers = preg_replace('/\D/', '', $input);
+        $numbers = str_split($numbers);
+        $numbers = array_reverse($numbers);
+
+        $verify = array_shift($numbers);
+
+        array_walk($numbers, function (&$number, $position) {
+            if (($position + 1) % 2 == 1) {
+                $n = $number * 2;
+                $number = intval($n > 9 ? ($n % 10) + ($n / 10) : $n);
+            }
+        });
+
+        $sum = array_sum($numbers);
+        return (ceil($sum / 10) * 10) - $sum == $verify;
+    }
+}
