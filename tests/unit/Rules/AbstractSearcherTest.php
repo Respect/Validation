b7db6b71e32bd36feea2d4983c916d8976f4be11
marcosptf <marcosptf@yahoo.com.br>
2015-10-31 17:09:20 -0200

    code refactory.

--- tests/unit/Rules/AbstractSearcherTest.php
+++ tests/unit/Rules/AbstractSearcherTest.php
@@ -19,77 +19,86 @@ namespace Respect\Validation\Rules;
 class AbstractSearcherTest extends \PHPUnit_Framework_TestCase
 {
 
+    private $testValidateTrue = true;
+    private $testValidateFalse = false;    
+    private $testValidateNull = null;
+    private $testValidateEmptyWithDoubleQuotes = "";
+    private $testValidateEmptyWithSingleQuotes = '';
+    private $testValidateArrayLangs = array("goLang", "java", "python", "cLang", "F#");
+    private $testValidateNotInArrayLangs = "js";
+    private $testValidateMockPath = "Respect\\Validation\\Rules\\AbstractSearcher";
+    
     public function testValidateMethodWhenArgsIsNullCompareIdenticalFalse()
     {
-        $compositeRuleMock = $this->getMockForAbstractClass('Respect\\Validation\\Rules\\AbstractSearcher');
-        $compositeRuleMock->compareIdentical = false;
-        $this->assertTrue($compositeRuleMock->validate(null));
+        $compositeRuleMock = $this->getMockForAbstractClass($this->testValidateMockPath);
+        $compositeRuleMock->compareIdentical = $this->testValidateFalse;
+        $this->assertTrue($compositeRuleMock->validate($this->testValidateNull));
     }
 
     public function testValidateMethodWhenArgsIsNullCompareIdenticalTrue()
     {
-        $compositeRuleMock = $this->getMockForAbstractClass('Respect\\Validation\\Rules\\AbstractSearcher');
-        $compositeRuleMock->compareIdentical = true;
-        $this->assertTrue($compositeRuleMock->validate(null));
+        $compositeRuleMock = $this->getMockForAbstractClass($this->testValidateMockPath);
+        $compositeRuleMock->compareIdentical = $this->testValidateTrue;
+        $this->assertTrue($compositeRuleMock->validate($this->testValidateNull));
     }
     
     public function testValidateMethodWhenArgsIsEmptyWithDoubleQuotesCompareIdenticalFalse()
     {
-        $compositeRuleMock = $this->getMockForAbstractClass('Respect\\Validation\\Rules\\AbstractSearcher');
-        $compositeRuleMock->compareIdentical = false;
-        $this->assertTrue($compositeRuleMock->validate(""));
+        $compositeRuleMock = $this->getMockForAbstractClass($this->testValidateMockPath);
+        $compositeRuleMock->compareIdentical = $this->testValidateFalse;
+        $this->assertTrue($compositeRuleMock->validate($this->testValidateEmptyWithDoubleQuotes));
     }
     
     public function testValidateMethodWhenArgsIsEmptyWithDoubleQuotesCompareIdenticalTrue()
     {
-        $compositeRuleMock = $this->getMockForAbstractClass('Respect\\Validation\\Rules\\AbstractSearcher');
-        $compositeRuleMock->compareIdentical = true;
-        $this->assertFalse($compositeRuleMock->validate(""));
+        $compositeRuleMock = $this->getMockForAbstractClass($this->testValidateMockPath);
+        $compositeRuleMock->compareIdentical = $this->testValidateTrue;
+        $this->assertFalse($compositeRuleMock->validate($this->testValidateEmptyWithDoubleQuotes));
     }    
     
     public function testValidateMethodWhenArgsIsEmptyWithSingleQuotesCompareIdenticalFalse()
     {
-        $compositeRuleMock = $this->getMockForAbstractClass('Respect\\Validation\\Rules\\AbstractSearcher');
-        $compositeRuleMock->compareIdentical = false;     
-        $this->assertTrue($compositeRuleMock->validate(''));
+        $compositeRuleMock = $this->getMockForAbstractClass($this->testValidateMockPath);
+        $compositeRuleMock->compareIdentical = $this->testValidateFalse;     
+        $this->assertTrue($compositeRuleMock->validate($this->testValidateEmptyWithSingleQuotes));
     }
     
     public function testValidateMethodWhenArgsIsEmptyWithSingleQuotesCompareIdenticalTrue()
     {
-        $compositeRuleMock = $this->getMockForAbstractClass('Respect\\Validation\\Rules\\AbstractSearcher');
-        $compositeRuleMock->compareIdentical = true;
-        $this->assertFalse($compositeRuleMock->validate(''));
+        $compositeRuleMock = $this->getMockForAbstractClass($this->testValidateMockPath);
+        $compositeRuleMock->compareIdentical = $this->testValidateTrue;
+        $this->assertFalse($compositeRuleMock->validate($this->testValidateEmptyWithSingleQuotes));
     }    
     
     public function testValidateMethodWhenArgsNotIsInArrayHaystackCompareIdenticalFalse()
     {
-        $compositeRuleMock = $this->getMockForAbstractClass('Respect\\Validation\\Rules\\AbstractSearcher');
-        $compositeRuleMock->haystack = array("goLang","java","python");
-        $compositeRuleMock->compareIdentical = false;
-        $this->assertFalse($compositeRuleMock->validate("js"));
+        $compositeRuleMock = $this->getMockForAbstractClass($this->testValidateMockPath);
+        $compositeRuleMock->haystack = $this->testValidateArrayLangs;
+        $compositeRuleMock->compareIdentical = $this->testValidateFalse;
+        $this->assertFalse($compositeRuleMock->validate($this->testValidateNotInArrayLangs));
     }
     
     public function testValidateMethodWhenArgsNotIsInArrayHaystackCompareIdenticalTrue()
     {
-        $compositeRuleMock = $this->getMockForAbstractClass('Respect\\Validation\\Rules\\AbstractSearcher');
-        $compositeRuleMock->haystack = array("goLang","java","python");
-        $compositeRuleMock->compareIdentical = true;
-        $this->assertFalse($compositeRuleMock->validate("js"));
+        $compositeRuleMock = $this->getMockForAbstractClass($this->testValidateMockPath);
+        $compositeRuleMock->haystack = $this->testValidateArrayLangs;
+        $compositeRuleMock->compareIdentical = $this->testValidateTrue;
+        $this->assertFalse($compositeRuleMock->validate($this->testValidateNotInArrayLangs));
     }    
     
     public function testValidateMethodWhenArgsIsInArrayHaystackCompareIdenticalFalse()
     {
-        $compositeRuleMock = $this->getMockForAbstractClass('Respect\\Validation\\Rules\\AbstractSearcher');
-        $compositeRuleMock->haystack = array("goLang","java","python");
-        $compositeRuleMock->compareIdentical = false;
-        $this->assertTrue($compositeRuleMock->validate("java"));
+        $compositeRuleMock = $this->getMockForAbstractClass($this->testValidateMockPath);
+        $compositeRuleMock->haystack = $this->testValidateArrayLangs;
+        $compositeRuleMock->compareIdentical = $this->testValidateFalse;
+        $this->assertTrue($compositeRuleMock->validate($this->testValidateArrayLangs[1]));
     }
     
     public function testValidateMethodWhenArgsIsInArrayHaystackCompareIdenticalTrue()
     {
-        $compositeRuleMock = $this->getMockForAbstractClass('Respect\\Validation\\Rules\\AbstractSearcher');
-        $compositeRuleMock->haystack = array("goLang","java","python");
-        $compositeRuleMock->compareIdentical = true;
-        $this->assertTrue($compositeRuleMock->validate("java"));
+        $compositeRuleMock = $this->getMockForAbstractClass($this->testValidateMockPath);
+        $compositeRuleMock->haystack = $this->testValidateArrayLangs;
+        $compositeRuleMock->compareIdentical = $this->testValidateTrue;
+        $this->assertTrue($compositeRuleMock->validate($this->testValidateArrayLangs[1]));
     }        
 }
