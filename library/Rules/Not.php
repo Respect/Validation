d1c3b2596ea6877308ae23a9f144f532e289a1bb
Caio César Tavares <caiotava@gmail.com>
2016-01-29 19:50:27 -0200

    Define names for the child of Not rule

--- library/Rules/Not.php
+++ library/Rules/Not.php
@@ -23,6 +23,13 @@ class Not extends AbstractRule
         $this->rule = $rule;
     }
 
+    public function setName($name)
+    {
+        $this->rule->setName($name);
+
+        return parent::setName($name);
+    }
+
     public function validate($input)
     {
         return (false == $this->rule->validate($input));
