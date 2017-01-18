5358ef32b95627d87a4621dbf44866a675497d64
Yosmany Garcia <yosmanyga@gmail.com>
2017-01-18 15:17:47 -0800

    Updated provinces of Cuba
    Updated provinces of Cuba according to the change of 2011

--- library/Rules/SubdivisionCode/CuSubdivisionCode.php
+++ library/Rules/SubdivisionCode/CuSubdivisionCode.php
@@ -36,8 +36,9 @@ class CuSubdivisionCode extends AbstractSearcher
         '12', // Granma
         '13', // Santiago de Cuba
         '14', // Guantanamo
+        '15', // Artemisa
+        '16', // Mayabeque
         '99', // Isla de la Juventud
-        '02', // La Habana
     ];
 
     public $compareIdentical = true;
