b43c9b103b1bad28af0113413260d5c825e00bad
Henrique Moody <henriquemoody@gmail.com>
2017-02-12 18:14:38 +0100

    Better message for Length rule
    Update exception message when minimum value and maximum value are the
    same.

--- library/Exceptions/LengthException.php
+++ library/Exceptions/LengthException.php
@@ -16,17 +16,20 @@ class LengthException extends ValidationException
     const BOTH = 0;
     const LOWER = 1;
     const GREATER = 2;
+    const EXACT = 3;
 
     public static $defaultTemplates = [
         self::MODE_DEFAULT => [
             self::BOTH => '{{name}} must have a length between {{minValue}} and {{maxValue}}',
             self::LOWER => '{{name}} must have a length greater than {{minValue}}',
             self::GREATER => '{{name}} must have a length lower than {{maxValue}}',
+            self::EXACT => '{{name}} must have a length of {{maxValue}}',
         ],
         self::MODE_NEGATIVE => [
             self::BOTH => '{{name}} must not have a length between {{minValue}} and {{maxValue}}',
             self::LOWER => '{{name}} must not have a length greater than {{minValue}}',
             self::GREATER => '{{name}} must not have a length lower than {{maxValue}}',
+            self::EXACT => '{{name}} must not have a length of {{maxValue}}',
         ],
     ];
 
@@ -34,8 +37,14 @@ class LengthException extends ValidationException
     {
         if (!$this->getParam('minValue')) {
             return static::GREATER;
-        } elseif (!$this->getParam('maxValue')) {
+        }
+
+        if (!$this->getParam('maxValue')) {
             return static::LOWER;
+        }
+
+        if ($this->getParam('minValue') == $this->getParam('maxValue')) {
+            return self::EXACT;
         }
 
         return static::BOTH;
