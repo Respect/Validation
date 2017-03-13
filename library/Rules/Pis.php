a0ba215524bd7743e4380f187853294af8a373d7
Bruno Koga <brunokoga187@gmail.com>
2017-03-13 18:51:55 -0300

    Create "Pis" rule

---
+++ library/Rules/Pis.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Rules;
+
+/**
+ * Validates a Brazilian PIS/NIS number.
+ *
+ * @author Bruno Koga <brunokoga187@gmail.com>
+ * @author Henrique Moody <henriquemoody@gmail.com>
+ */
+class Pis extends AbstractRule
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($input)
+    {
+        if (!is_scalar($input)) {
+            return false;
+        }
+
+        $digits = preg_replace('/\D/', '', $input);
+        if (mb_strlen($digits) != 11 || preg_match("/^{$digits[0]}{11}$/", $digits)) {
+            return false;
+        }
+
+        $multipliers = [3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+
+        $summation = 0;
+        for ($position = 0; $position < 10; ++$position) {
+            $summation += $digits[$position] * $multipliers[$position];
+        }
+
+        $checkDigit = (int) $digits[10];
+
+        $modulo = $summation % 11;
+
+        return $checkDigit === (($modulo < 2) ? 0 : 11 - $modulo);
+    }
+}
--- library/Validator.php
+++ library/Validator.php
@@ -119,6 +119,7 @@ use Respect\Validation\Rules\Key;
  * @method static Validator pesel()
  * @method static Validator phone()
  * @method static Validator phpLabel()
+ * @method static Validator pis()
  * @method static Validator positive()
  * @method static Validator postalCode(string $countryCode)
  * @method static Validator primeNumber()
