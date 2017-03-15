b239ce1cca0c345fcd7bf349c9b469272f4e9179
Bruno Koga <brunokoga187@gmail.com>
2017-03-15 14:14:13 -0300

    Fix copyright

--- library/Rules/Pis.php
+++ library/Rules/Pis.php
@@ -3,6 +3,8 @@
 /*
  * This file is part of Respect/Validation.
  *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
  * For the full copyright and license information, please view the "LICENSE.md"
  * file that was distributed with this source code.
  */
