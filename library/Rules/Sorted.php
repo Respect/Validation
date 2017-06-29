52c8541b9eb7222df7b366bf71c4b5b88408eed2
Mikhail Vyrtsev <reeywhaar@gmail.com>
2017-06-29 05:18:02 +0300

    New rule: Sorted

---
+++ library/Rules/Sorted.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Rules;
+
+class Sorted extends AbstractRule
+{
+	public function __construct(callable $fn = null, bool $ascending = true)
+	{
+		$this->fn = $fn ?? function($x){ return $x;};
+		$this->ascending = $ascending;
+	}
+
+    public function validate($input)
+    {
+		$count = count($input);
+		if($count < 2){
+			return true;
+		};
+		for($i = 1; $i < $count; $i++){
+			$cmp = $this->ascending === true
+				? ($this->fn)($input[$i]) > ($this->fn)($input[$i - 1])
+				: ($this->fn)($input[$i]) < ($this->fn)($input[$i - 1]);
+			if($cmp === false) return false;
+		}
+		return true;
+    }
+}
