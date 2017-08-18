f25b12f4e79c6e2adf95df1d06a5113d5a46eb9a
mazanax <mazanax@yandex.ru>
2017-08-18 14:58:10 +0300

    Added missed headers

--- library/Rules/Luhn.php
+++ library/Rules/Luhn.php
@@ -1,9 +1,21 @@
 <?php
 
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
 namespace Respect\Validation\Rules;
 
 class Luhn extends AbstractRule
 {
+    /**
+     * {@inheritdoc}
+     */
     public function validate($input)
     {
         $input = preg_replace('([^0-9])', '', $input);
