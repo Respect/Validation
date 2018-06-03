f9536d5537c1ed9bacbc71cfbc1576c14517395d
Danilo Benevides <danilobenevides01@gmail.com>
2018-06-03 15:07:02 -0300

    Apply contribution guidelines to "Lowercase" rule

--- library/Rules/Lowercase.php
+++ library/Rules/Lowercase.php
@@ -13,10 +13,29 @@ declare(strict_types=1);
 
 namespace Respect\Validation\Rules;
 
-class Lowercase extends AbstractRule
+use function is_string;
+use function mb_detect_encoding;
+use function mb_strtolower;
+
+/**
+ * Validates whether the characters in the input are lowercase.
+ *
+ * @author Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ * @author Danilo Benevides <danilobenevides01@gmail.com>
+ * @author Henrique Moody <henriquemoody@gmail.com>
+ * @author Jean Pimentel <jeanfap@gmail.com>
+ */
+final class Lowercase extends AbstractRule
 {
+    /**
+     * {@inheritdoc}
+     */
     public function validate($input): bool
     {
+        if (!is_string($input)) {
+            return false;
+        }
+
         return $input === mb_strtolower($input, mb_detect_encoding($input));
     }
 }
