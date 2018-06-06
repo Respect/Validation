5fc04558d6a3b428399785ccebd2ecbbe1b64721
Danilo Benevides <danilobenevides01@gmail.com>
2018-06-05 23:22:55 -0300

    Apply contribution guidelines to "Odd" rule
    Also does not allow validation of non-numeric values.
    
    Co-Authored-By: Henrique Moody <henriquemoody@gmail.com>

--- library/Rules/Odd.php
+++ library/Rules/Odd.php
@@ -13,10 +13,28 @@ declare(strict_types=1);
 
 namespace Respect\Validation\Rules;
 
-class Odd extends AbstractRule
+use function filter_var;
+use function is_numeric;
+
+/**
+ * Validates whether the input is an odd number or not.
+ *
+ * @author Danilo Benevides <danilobenevides01@gmail.com>
+ * @author Henrique Moody <henriquemoody@gmail.com>
+ * @author Jean Pimentel <jeanfap@gmail.com>
+ */
+final class Odd extends AbstractRule
 {
     public function validate($input): bool
     {
+        if (!is_numeric($input)) {
+            return false;
+        }
+
+        if (!filter_var($input, FILTER_VALIDATE_INT)) {
+            return false;
+        }
+
         return 0 !== (int) $input % 2;
     }
 }
