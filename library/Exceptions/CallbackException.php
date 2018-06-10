1d171b3a8a4f00708c6e41932771e23c110eb6e7
William Espindola <oi@williamespindola.com.br>
2018-06-10 00:49:57 -0300

    Apply contribution guidelines to "Callback" rule

--- library/Exceptions/CallbackException.php
+++ library/Exceptions/CallbackException.php
@@ -13,8 +13,16 @@ declare(strict_types=1);
 
 namespace Respect\Validation\Exceptions;
 
-class CallbackException extends NestedValidationException
+/**
+ * @author Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ * @author Henrique Moody <henriquemoody@gmail.com>
+ * @author William Espindola <oi@williamespindola.com.br>
+ */
+final class CallbackException extends NestedValidationException
 {
+    /**
+     * {@inheritdoc}
+     */
     public static $defaultTemplates = [
         self::MODE_DEFAULT => [
             self::STANDARD => '{{name}} must be valid',
