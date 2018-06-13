755189c41149c255f79bcc3ba5602c04a4f11d59
William Espindola <oi@williamespindola.com.br>
2018-06-13 00:15:25 -0300

    Apply contribution guidelines to "CreditCard" rule

--- library/Exceptions/CreditCardException.php
+++ library/Exceptions/CreditCardException.php
@@ -13,10 +13,18 @@ declare(strict_types=1);
 
 namespace Respect\Validation\Exceptions;
 
-class CreditCardException extends ValidationException
+/**
+ * @author Henrique Moody <henriquemoody@gmail.com>
+ * @author Jean Pimentel <jeanfap@gmail.com>
+ * @author William Espindola <oi@williamespindola.com.br>
+ */
+final class CreditCardException extends ValidationException
 {
     public const BRANDED = 'branded';
 
+    /**
+     * {@inheritdoc}
+     */
     public static $defaultTemplates = [
         self::MODE_DEFAULT => [
             self::STANDARD => '{{name}} must be a valid Credit Card number',
