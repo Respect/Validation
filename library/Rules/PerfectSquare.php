e1697af09a309665f2ce4e7176aebd573685113a
Danilo Benevides <danilobenevides01@gmail.com>
2018-06-18 23:14:54 -0300

    Apply contribution guidelines to PerfectSquare rule

--- library/Rules/PerfectSquare.php
+++ library/Rules/PerfectSquare.php
@@ -13,8 +13,21 @@ declare(strict_types=1);
 
 namespace Respect\Validation\Rules;
 
-class PerfectSquare extends AbstractRule
+use function floor;
+use function is_numeric;
+use function sqrt;
+
+/**
+ * @author Danilo Benevides <danilobenevides01@gmail.com>
+ * @author Henrique Moody <henriquemoody@gmail.com>
+ * @author Kleber Hamada Sato <kleberhs007@yahoo.com>
+ * @author Nick Lombard <github@jigsoft.co.za>
+ */
+final class PerfectSquare extends AbstractRule
 {
+    /**
+     * {@inheritdoc}
+     */
     public function validate($input): bool
     {
         return is_numeric($input) && floor(sqrt((float) $input)) == sqrt((float) $input);
