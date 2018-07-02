667ed36b4868e785d52d815181ff00c798a61be7
Henrique Moody <henriquemoody@gmail.com>
2018-07-02 08:22:41 +0200

    Fix grammatical errors in URL exception message
    Although the "U" at the first of URL is a vowel, the abbreviation is
    pronounced "you-are-ell". Because the letter "Y" is a consonant in this
    case, we should use "a" instead of "an".
    
    Link: https://itknowledgeexchange.techtarget.com/writing-for-business/which-is-correct-a-url-or-an-url/
    
    Signed-off-by: Henrique Moody <henriquemoody@gmail.com>

--- library/Exceptions/UrlException.php
+++ library/Exceptions/UrlException.php
@@ -15,10 +15,10 @@ class UrlException extends ValidationException
 {
     public static $defaultTemplates = [
         self::MODE_DEFAULT => [
-            self::STANDARD => '{{name}} must be an URL',
+            self::STANDARD => '{{name}} must be a URL',
         ],
         self::MODE_NEGATIVE => [
-            self::STANDARD => '{{name}} must not be an URL',
+            self::STANDARD => '{{name}} must not be a URL',
         ],
     ];
 }
