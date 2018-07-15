a4c4834e396b4abbed1fbcb5ab7dc35d2195d4c9
Danilo Benevides <danilobenevides01@gmail.com>
2018-07-15 00:29:23 -0300

    Apply contribution guidelines to "Multiple" rule

--- library/Exceptions/MultipleException.php
+++ library/Exceptions/MultipleException.php
@@ -13,8 +13,16 @@ declare(strict_types=1);
 
 namespace Respect\Validation\Exceptions;
 
-class MultipleException extends ValidationException
+/**
+ * @author Danilo Benevides <danilobenevides01@gmail.com>
+ * @author Henrique Moody <henriquemoody@gmail.com>
+ * @author Jean Pimentel <jeanfap@gmail.com>
+ */
+final class MultipleException extends ValidationException
 {
+    /**
+     * {@inheritdoc}
+     */
     public static $defaultTemplates = [
         self::MODE_DEFAULT => [
             self::STANDARD => '{{name}} must be multiple of {{multipleOf}}',
