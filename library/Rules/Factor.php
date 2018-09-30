be1bb1cc72cb2ebe20c6ec2c313bfdff925f4613
Danilo Correa <danilosilva87@gmail.com>
2018-09-30 10:43:15 -0300

    Apply contribution guidelines to "Factor" rule

--- library/Rules/Factor.php
+++ library/Rules/Factor.php
@@ -13,26 +13,37 @@ declare(strict_types=1);
 
 namespace Respect\Validation\Rules;
 
-use Respect\Validation\Exceptions\ComponentException;
-use function Respect\Stringifier\stringify;
+use function abs;
+use function is_integer;
+use function is_numeric;
 
 /**
+ * Validates if the input is a factor of the defined dividend.
+ *
+ * @author Danilo Correa <danilosilva87@gmail.com>
  * @author David Meister <thedavidmeister@gmail.com>
+ * @author Henrique Moody <henriquemoody@gmail.com>
  */
-class Factor extends AbstractRule
+final class Factor extends AbstractRule
 {
-    public $dividend;
+    /**
+     * @var int
+     */
+    private $dividend;
 
-    public function __construct($dividend)
+    /**
+     * Initializes the rule.
+     *
+     * @param int $dividend
+     */
+    public function __construct(int $dividend)
     {
-        if (!is_numeric($dividend) || (int) $dividend != $dividend) {
-            $message = 'Dividend %s must be an integer';
-            throw new ComponentException(sprintf($message, stringify($dividend)));
-        }
-
-        $this->dividend = (int) $dividend;
+        $this->dividend = $dividend;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function validate($input): bool
     {
         // Every integer is a factor of zero, and zero is the only integer that
