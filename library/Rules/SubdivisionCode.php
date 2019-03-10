718bacad046b5f636c33ab0f9d37bca4dbacb149
Henrique Moody <henriquemoody@gmail.com>
2019-03-11 00:40:14 +0100

    Remove subdivision code rules per country
    There is no much benefit from having individual rules for each country's
    subdivision, quite the opposite. It increases the amount of code and
    makes it hard to change the implementation of these rules. Right now,
    the only sane way to change those rules is with a customized script.
    
    This commit will remove the Subdivision Code rules per country and
    instead will put that information into JSON files.
    
    We both wouldn't like to keep this in this library anymore, and we are
    considering having another library to deal with this data [1], but since
    it seems like it may take some time, looks better to do it temporarily
    here.
    
    [1]: https://github.com/sokil/php-isocodes/issues/12
    
    Co-authored-by: Mazen Touati <mazen_touati@hotmail.com>
    Signed-off-by: Henrique Moody <henriquemoody@gmail.com>

--- library/Rules/SubdivisionCode.php
+++ library/Rules/SubdivisionCode.php
@@ -13,6 +13,9 @@ declare(strict_types=1);
 
 namespace Respect\Validation\Rules;
 
+use Respect\Validation\Helpers\Subdivisions;
+use function array_keys;
+
 /**
  * Validates country subdivision codes according to ISO 3166-2.
  *
@@ -20,14 +23,33 @@ namespace Respect\Validation\Rules;
  * @see http://www.geonames.org/countries/
  *
  * @author Henrique Moody <henriquemoody@gmail.com>
+ * @author Mazen Touati <mazen_touati@hotmail.com>
  */
-final class SubdivisionCode extends AbstractLocaleWrapper
+final class SubdivisionCode extends AbstractSearcher
 {
+    /**
+     * @var string
+     */
+    private $countryName;
+
+    /**
+     * @var string[]
+     */
+    private $subdivisions;
+
+    public function __construct(string $countryCode)
+    {
+        $subdivisions = new Subdivisions($countryCode);
+
+        $this->countryName = $subdivisions->getCountry();
+        $this->subdivisions = array_keys($subdivisions->getSubdivisions());
+    }
+
     /**
      * {@inheritDoc}
      */
-    protected function getSuffix(): string
+    protected function getDataSource(): array
     {
-        return 'SubdivisionCode';
+        return $this->subdivisions;
     }
 }
