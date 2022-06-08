e219da29453bcb55347d2db8cf58943968e3f2a3
Gonçalo Andrade <goncalo.andrade@premium-minds.com>
2022-06-08 14:29:19 +0100

    Add rule for validating portuguese fiscal numbers

---
+++ library/Exceptions/PortugueseNifException.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Respect\Validation\Exceptions;
+
+/**
+ * @author Gonçalo Andrade <goncalo.andrade95@gmail.com>
+ */
+final class PortugueseNifException extends ValidationException
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $defaultTemplates = [
+        self::MODE_DEFAULT => [
+            self::STANDARD => '{{name}} must be a Portuguese NIF',
+        ],
+        self::MODE_NEGATIVE => [
+            self::STANDARD => '{{name}} must not be a Portuguese NIF'
+        ]
+    ];
+}
