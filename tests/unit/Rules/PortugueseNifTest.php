1481c8e1e5e71aa08c39f85af604f702deacc28f
Alexandre Gomes Gaigalas <alganet+github@gmail.com>
2023-02-13 19:24:37 -0300

    Updated docheaders

--- tests/unit/Rules/PortugueseNifTest.php
+++ tests/unit/Rules/PortugueseNifTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Respect/Validation.
+ *
+ * (c) Alexandre Gomes Gaigalas <alganet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Respect\Validation\Rules;
