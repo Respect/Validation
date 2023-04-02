445af454fdc415b49b11fa967f339cde8af4e724
Henrique Moody <henriquemoody@gmail.com>
2023-04-02 15:28:23 +0200

    Create a class to help test the Callable rule
    With that, the tests will be more straightforward, and we won't need to
    use the test class in the data providers. That will help us later
    because, on PHPUnit 10, all data providers need to be static.
    
    Signed-off-by: Henrique Moody <henriquemoody@gmail.com>

---
+++ tests/library/Stubs/WithMethods.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Copyright (c) Alexandre Gomes Gaigalas <alganet@gmail.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+declare(strict_types=1);
+
+namespace Respect\Validation\Test\Stubs;
+
+final class WithMethods
+{
+    public function publicMethod(): void
+    {
+    }
+
+    public static function publicStaticMethod(): void
+    {
+    }
+}
