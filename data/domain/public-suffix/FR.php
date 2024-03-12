ef5644c2cd23540e61ace24df0ae65a2e121a5bd
The Respect Panda <therespectpanda@gmail.com>
2024-03-12 00:05:37 +0000

    Update Regional Information

--- data/domain/public-suffix/FR.php
+++ data/domain/public-suffix/FR.php
@@ -2,25 +2,14 @@
 // Copyright (c) https://publicsuffix.org
 // SPDX-License-Identifier: MPL-2.0-no-copyleft-exception
 return [
-    'AEROPORT.FR',
     'ASSO.FR',
-    'AVOCAT.FR',
     'AVOUES.FR',
     'CCI.FR',
-    'CHAMBAGRI.FR',
-    'CHIRURGIENS-DENTISTES.FR',
     'COM.FR',
-    'EXPERTS-COMPTABLES.FR',
-    'GEOMETRE-EXPERT.FR',
     'GOUV.FR',
     'GRETA.FR',
     'HUISSIER-JUSTICE.FR',
-    'MEDECIN.FR',
     'NOM.FR',
-    'NOTAIRES.FR',
-    'PHARMACIEN.FR',
-    'PORT.FR',
     'PRD.FR',
     'TM.FR',
-    'VETERINAIRE.FR',
 ];
