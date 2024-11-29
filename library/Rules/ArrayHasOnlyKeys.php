b48c21429d1e0a92079f1e68f78a42f547e13746
Henrique Moody <henriquemoody@gmail.com>
2024-11-29 16:24:04 +0100

    Create a few rules to check keys in an array

---
+++ library/Rules/ArrayHasOnlyKeys.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * Copyright (c) Alexandre Gomes Gaigalas <alganet@gmail.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+declare(strict_types=1);
+
+namespace Respect\Validation\Rules;
+
+use Respect\Validation\Helpers\CanBindEvaluateRule;
+use Respect\Validation\Message\Template;
+use Respect\Validation\Result;
+use Respect\Validation\Rules\Core\Standard;
+
+use function array_diff;
+use function array_keys;
+use function count;
+
+#[Template(
+    'Must have only the keys {{keys}} in {{name}}',
+    'Must have not only the keys {{keys}} in {{name}}',
+)]
+final class ArrayHasOnlyKeys extends Standard
+{
+    use CanBindEvaluateRule;
+
+    /** @var array<string|int> */
+    private readonly array $keys;
+
+    public function __construct(int|string ...$keys)
+    {
+        $this->keys = $keys;
+    }
+
+    public function evaluate(mixed $input): Result
+    {
+        $result = $this->bindEvaluate(new ArrayType(), $this, $input);
+        if (!$result->isValid) {
+            return $result;
+        }
+
+        if (count(array_diff(array_keys($input), $this->keys)) > 0) {
+            return Result::failed($input, $this, ['keys' => $this->keys]);
+        }
+
+        return Result::passed($input, $this, ['keys' => $this->keys]);
+    }
+}
