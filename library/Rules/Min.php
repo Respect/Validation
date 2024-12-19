0507d649e87d769eebc08b42dca996a66ce6ebbe
Dominick Johnson <dominick.johnson@tylertech.com>
2024-12-19 14:13:48 -0700

    Extract common functionality from `Min` and `Max` into abstract base class.
    This base class could also be used for other aggregate operations on arrays, e.g. a sum. (It can't be used for `Length` though, as we would not be able to validate a length of 0.)

--- library/Rules/Min.php
+++ library/Rules/Min.php
@@ -12,35 +12,18 @@ namespace Respect\Validation\Rules;
 use Attribute;
 use Respect\Validation\Message\Template;
 use Respect\Validation\Result;
-use Respect\Validation\Rules\Core\FilteredNonEmptyArray;
+use Respect\Validation\Rules\Core\ArrayAggregateFunction;
 
-use function array_map;
 use function min;
 
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 #[Template('The minimum from', 'The minimum from')]
-final class Min extends FilteredNonEmptyArray
+final class Min extends ArrayAggregateFunction
 {
-    /** @param non-empty-array<mixed> $input */
-    protected function evaluateNonEmptyArray(array $input): Result
+    protected string $idPrefix = 'min';
+
+    protected function extractAggregate(array $input): mixed
     {
-        $min = min($input);
-
-        return $this->enrichResult($input, $this->rule->evaluate($min));
-    }
-
-    private function enrichResult(mixed $input, Result $result): Result
-    {
-        if (!$result->allowsSubsequent()) {
-            return $result
-                ->withInput($input)
-                ->withChildren(
-                    ...array_map(fn(Result $child) => $this->enrichResult($input, $child), $result->children)
-                );
-        }
-
-        return (new Result($result->isValid, $input, $this, id: $result->id))
-            ->withPrefixedId('min')
-            ->withSubsequent($result->withInput($input));
+        return min($input);
     }
 }
