6d3b518185310bd235aca7d69f855dea41d835d9
Dominick Johnson <dmjohnsson23@users.noreply.github.com>
2024-12-19 14:13:48 -0700

    Abstract common functionality from `Min` and `Max`
    This base class could also be used for other aggregate operations on
    arrays, e.g. a sum. (It can't be used for `Length` though, as we would
    not be able to validate a length of 0.).

--- library/Rules/Max.php
+++ library/Rules/Max.php
@@ -11,36 +11,19 @@ namespace Respect\Validation\Rules;
 
 use Attribute;
 use Respect\Validation\Message\Template;
-use Respect\Validation\Result;
-use Respect\Validation\Rules\Core\FilteredNonEmptyArray;
+use Respect\Validation\Rules\Core\ArrayAggregateFunction;
 
-use function array_map;
 use function max;
 
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 #[Template('The maximum of', 'The maximum of')]
-final class Max extends FilteredNonEmptyArray
+final class Max extends ArrayAggregateFunction
 {
+    protected string $idPrefix = 'max';
+
     /** @param non-empty-array<mixed> $input */
-    protected function evaluateNonEmptyArray(array $input): Result
+    protected function extractAggregate(array $input): mixed
     {
-        $max = max($input);
-
-        return $this->enrichResult($input, $this->rule->evaluate($max));
-    }
-
-    private function enrichResult(mixed $input, Result $result): Result
-    {
-        if (!$result->allowsSubsequent()) {
-            return $result
-                ->withInput($input)
-                ->withChildren(
-                    ...array_map(fn(Result $child) => $this->enrichResult($input, $child), $result->children)
-                );
-        }
-
-        return (new Result($result->isValid, $input, $this, id: $result->id))
-            ->withPrefixedId('max')
-            ->withSubsequent($result->withInput($input));
+        return max($input);
     }
 }
