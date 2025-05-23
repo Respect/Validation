7d62f92c7ce02f758794ffb8fbc91bb94b42bca3
steven.lewis <stevenlewis@gowebprint.com>
2025-05-23 15:12:49 +0100

    Extend UUID rule support to versions 1 through 8
    Updated the `Uuid` rule to validate UUID versions 1 to 8 using the
    `ramsey/uuid` library, replacing manual pattern checks. Updated tests
    and documentation accordingly, ensuring compatibility with string and
    UuidInterface` inputs.
    
    Co-authored-by: Henrique Moody <henriquemoody@gmail.com>

--- tests/unit/Rules/UuidTest.php
+++ tests/unit/Rules/UuidTest.php
@@ -12,11 +12,16 @@ namespace Respect\Validation\Rules;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Test;
+use Ramsey\Uuid\Uuid as RamseyUuid;
+use Ramsey\Uuid\UuidInterface;
 use Respect\Validation\Exceptions\InvalidRuleConstructorException;
 use Respect\Validation\Test\RuleTestCase;
 use stdClass;
 
+use function array_keys;
 use function random_int;
+use function sprintf;
+use function str_replace;
 
 use const PHP_INT_MAX;
 use const PHP_INT_MIN;
@@ -25,27 +30,24 @@ use const PHP_INT_MIN;
 #[CoversClass(Uuid::class)]
 final class UuidTest extends RuleTestCase
 {
-    private const UUID_VERSION_1 = 'e4eaaaf2-d142-11e1-b3e4-080027620cdd';
-    private const UUID_VERSION_3 = '11a38b9a-b3da-360f-9353-a5a725514269';
-    private const UUID_VERSION_4 = '25769c6c-d34d-4bfe-ba98-e0ee856f3e7a';
-    private const UUID_VERSION_5 = 'c4a760a8-dbcf-5254-a0d9-6a4474bd1b62';
-
-    #[Test]
-    public function itShouldThrowExceptionWhenVersionIsTwo(): void
-    {
-        self::expectException(InvalidRuleConstructorException::class);
-        self::expectExceptionMessage('Only versions 1, 3, 4, and 5 are supported: 2 given');
-
-        new Uuid(2);
-    }
+    private const ALL_VERSIONS = [
+        1 => 'e4eaaaf2-d142-11e1-b3e4-080027620cdd',
+        2 => '000003e8-3702-21f0-9f00-325096b39f47',
+        3 => '11a38b9a-b3da-360f-9353-a5a725514269',
+        4 => '25769c6c-d34d-4bfe-ba98-e0ee856f3e7a',
+        5 => 'c4a760a8-dbcf-5254-a0d9-6a4474bd1b62',
+        6 => '1f037034-88c0-61d0-a876-e4456153c969',
+        7 => '0196f7d6-f570-7041-8106-c0011f7a9bcd',
+        8 => '00112233-4455-8677-8899-aabbccddeeff',
+    ];
 
     #[Test]
     public function itShouldThrowExceptionWhenVersionIsGreaterThanFive(): void
     {
-        $version = random_int(6, PHP_INT_MAX);
+        $version = random_int(8, PHP_INT_MAX);
 
         self::expectException(InvalidRuleConstructorException::class);
-        self::expectExceptionMessage('Only versions 1, 3, 4, and 5 are supported: ' . $version . ' given');
+        self::expectExceptionMessage('Only versions 1 to 8 are supported: ' . $version . ' given');
 
         new Uuid($version);
     }
@@ -56,59 +58,66 @@ final class UuidTest extends RuleTestCase
         $version = random_int(PHP_INT_MIN, 0);
 
         self::expectException(InvalidRuleConstructorException::class);
-        self::expectExceptionMessage('Only versions 1, 3, 4, and 5 are supported: ' . $version . ' given');
+        self::expectExceptionMessage('Only versions 1 to 8 are supported: ' . $version . ' given');
 
         new Uuid($version);
     }
 
-    /** @return iterable<array{Uuid, mixed}> */
+    /** @return iterable<array{Uuid, string|UuidInterface}> */
     public static function providerForValidInput(): iterable
     {
-        $sut = new Uuid();
-
-        return [
-            'any version with version 1' => [$sut, self::UUID_VERSION_1],
-            'any version with version 3' => [$sut, self::UUID_VERSION_3],
-            'any version with version 4' => [$sut, self::UUID_VERSION_4],
-            'any version with version 5' => [$sut, self::UUID_VERSION_5],
-            'version 1 with version 1' => [new Uuid(1), self::UUID_VERSION_1],
-            'version 3 with version 3' => [new Uuid(3), self::UUID_VERSION_3],
-            'version 4 with version 4' => [new Uuid(4), self::UUID_VERSION_4],
-            'version 5 with version 5' => [new Uuid(5), self::UUID_VERSION_5],
-        ];
+        foreach (self::ALL_VERSIONS as $version => $string) {
+            yield sprintf('default with UUID version %d string', $version) => [new Uuid(), $string];
+            yield sprintf('default with unformatted UUID version %d string', $version) => [
+                new Uuid(),
+                str_replace('-', '', $string),
+            ];
+            yield sprintf('default with UUID version %d object', $version) => [
+                new Uuid(),
+                RamseyUuid::fromString($string),
+            ];
+            yield sprintf('expected version %1$d with UUID v%1$d string', $version) => [
+                new Uuid($version),
+                $string,
+            ];
+            yield sprintf('expected version %1$d with unformatted UUID v%1$d string', $version) => [
+                new Uuid($version),
+                str_replace('-', '', $string),
+            ];
+            yield sprintf('expected version %1$d with UUID v%1$d object', $version) => [
+                new Uuid($version),
+                RamseyUuid::fromString($string),
+            ];
+        }
     }
 
     /** @return iterable<array{Uuid, mixed}> */
     public static function providerForInvalidInput(): iterable
     {
-        $sut = new Uuid();
-        $sutVersion1 = new Uuid(1);
-        $sutVersion3 = new Uuid(3);
-        $sutVersion4 = new Uuid(4);
-        $sutVersion5 = new Uuid(5);
+        yield 'empty' => [new Uuid(), ''];
+        yield 'nil/empty' => [new Uuid(), '00000000-0000-0000-0000-000000000000'];
+        yield 'not UUID' => [new Uuid(), 'Not an UUID'];
+        yield 'invalid UUID' => [new Uuid(), 'g71a18f4-3a13-11e7-a919-92ebcb67fe33'];
+        yield 'array' => [new Uuid(), []];
+        yield 'boolean true' => [new Uuid(), true];
+        yield 'boolean false' => [new Uuid(), false];
+        yield 'object' => [new Uuid(), new stdClass()];
 
-        return [
-            'empty' => [$sut, ''],
-            'nil/empty' => [$sut, '00000000-0000-0000-0000-000000000000'],
-            'not UUID' => [$sut, 'Not an UUID'],
-            'invalid UUID' => [$sut, 'g71a18f4-3a13-11e7-a919-92ebcb67fe33'],
-            'invalid format' => [$sut, 'a71a18f43a1311e7a91992ebcb67fe33'],
-            'version 1 with version 3' => [$sutVersion1, self::UUID_VERSION_3],
-            'version 1 with version 4' => [$sutVersion1, self::UUID_VERSION_4],
-            'version 1 with version 5' => [$sutVersion1, self::UUID_VERSION_5],
-            'version 3 with version 1' => [$sutVersion3, self::UUID_VERSION_1],
-            'version 3 with version 4' => [$sutVersion3, self::UUID_VERSION_4],
-            'version 3 with version 5' => [$sutVersion3, self::UUID_VERSION_5],
-            'version 4 with version 1' => [$sutVersion4, self::UUID_VERSION_1],
-            'version 4 with version 3' => [$sutVersion4, self::UUID_VERSION_3],
-            'version 4 with version 5' => [$sutVersion4, self::UUID_VERSION_5],
-            'version 5 with version 1' => [$sutVersion5, self::UUID_VERSION_1],
-            'version 5 with version 3' => [$sutVersion5, self::UUID_VERSION_3],
-            'version 5 with version 4' => [$sutVersion5, self::UUID_VERSION_4],
-            'array' => [$sut, []],
-            'boolean true' => [$sut, true],
-            'boolean false' => [$sut, false],
-            'object' => [$sut, new stdClass()],
-        ];
+        foreach (self::ALL_VERSIONS as $versionA => $string) {
+            foreach (array_keys(self::ALL_VERSIONS) as $versionB) {
+                if ($versionA === $versionB) {
+                    continue;
+                }
+
+                yield sprintf('expected version %1$d with UUID version %2$d string', $versionA, $versionB) => [
+                    new Uuid($versionB),
+                    $string,
+                ];
+                yield sprintf('expected version %1$d with UUID version %2$d object', $versionA, $versionB) => [
+                    new Uuid($versionB),
+                    RamseyUuid::fromString($string),
+                ];
+            }
+        }
     }
 }
