6dbea1186ec20d8f6881b90e740a357a86269125
Davide Spagnoli <davide.spagnoli@adam-italia.it>
2025-11-03 12:28:55 +0100

    typo
    Corrected a typo in the test method documentation.

--- tests/unit/Helpers/CanValidateDateTimeTest.php
+++ tests/unit/Helpers/CanValidateDateTimeTest.php
@@ -45,7 +45,7 @@ final class CanValidateDateTimeTest extends TestCase
     /**
      * @test isDateTime for month, when in "overflow" days (i.e. '2' when I run the test on 31st of any month)
      * if PECL timecop was installed we could have used "timecop_freeze(new DateTime('2025-10-31 12:00:00'));" before the assertion and "timecop_return();" after.
-     * without PECT timecop this test checks the fixed bug only on 29th, 30th and 31st of every month.
+     * without PECL timecop this test checks the fixed bug only on 29th, 30th and 31st of every month.
      */
     public function shouldFindWhenValueIsDateTimeOnOverflowDay(): void
     {
