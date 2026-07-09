a90e1b00e226c51b8ccf4d15f824df398ce51342
The Respect Panda <therespectpanda@gmail.com>
2026-07-09 00:37:55 +0000

    Update Regional Information

--- data/domain/public-suffix/AT.php
+++ data/domain/public-suffix/AT.php
@@ -3,6 +3,6 @@
 // SPDX-License-Identifier: MPL-2.0-no-copyleft-exception
 return [
     'exceptions' => [],
-    'rules' => ['123WEBSEITE.AT', '12HP.AT', '2IX.AT', '4.AT', '4LIMA.AT', 'AC.AT', 'BIZ.AT', 'CO.AT', 'DNSHOME.AT', 'FUTUREHOSTING.AT', 'FUTUREMAILING.AT', 'GV.AT', 'INFO.AT', 'LIMA-CITY.AT', 'MY.AT', 'MYSPREADSHOP.AT', 'OR.AT', 'PRIV.AT', 'STH.AC.AT', 'WIEN.FUNKFEUER.AT'],
+    'rules' => ['123WEBSEITE.AT', '12HP.AT', '2IX.AT', '4.AT', '4LIMA.AT', 'AC.AT', 'CO.AT', 'DNSHOME.AT', 'FUTUREHOSTING.AT', 'FUTUREMAILING.AT', 'GV.AT', 'LIMA-CITY.AT', 'MY.AT', 'MYSPREADSHOP.AT', 'OR.AT', 'PRIV.AT', 'STH.AC.AT', 'WIEN.FUNKFEUER.AT'],
     'wildcards' => ['EX.FUTURECMS.AT', 'EX.ORTSINFO.AT', 'FUTURECMS.AT', 'IN.FUTURECMS.AT', 'KUNDEN.ORTSINFO.AT'],
 ];
