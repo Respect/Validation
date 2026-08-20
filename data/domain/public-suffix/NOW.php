c92184224a113a113333fe9d460cecab1873d6c0
The Respect Panda <therespectpanda@gmail.com>
2026-08-20 00:12:30 +0000

    Update Regional Information

--- data/domain/public-suffix/NOW.php
+++ data/domain/public-suffix/NOW.php
@@ -3,6 +3,6 @@
 // SPDX-License-Identifier: MPL-2.0-no-copyleft-exception
 return [
     'exceptions' => [],
-    'rules' => ['DYN.NOW'],
+    'rules' => ['DYN.NOW', 'HERE.NOW'],
     'wildcards' => [],
 ];
